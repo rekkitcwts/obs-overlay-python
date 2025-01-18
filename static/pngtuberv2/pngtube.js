eac99ddf11c241ac52455f33bd544df9fe59b92c
Dynse Clyde Sacote <clydedatastruct@gmx.com>
2025-01-19 01:26:02 +0800

    Fixed POST link

--- static/pngtuberv2/pngtube.js
+++ static/pngtuberv2/pngtube.js
@@ -92,7 +92,7 @@ navigator.mediaDevices.enumerateDevices()
                 /*if (socket.connected) {
                     socket.send({ volume: volume }); // Send volume data
                 }*/
-                fetch('/../display', {
+                fetch('/pngtuber/v2/display', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
