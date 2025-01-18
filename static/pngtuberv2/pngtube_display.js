2b43fc53630c898f708188bdbb46ceb3dde8840d
Dynse Clyde Sacote <clydedatastruct@gmx.com>
2025-01-19 02:13:56 +0800

    Readded code for display side

--- static/pngtuberv2/pngtube_display.js
+++ static/pngtuberv2/pngtube_display.js
@@ -3,6 +3,7 @@
 // https://jameshfisher.com/2021/01/18/measuring-audio-volume-in-javascript/
 
 var volume;
+var emotion;
 // var minVolumeThreshold = 0.03; //Redundant...
 var softVolumeThreshold = 0.03;
 var mediumVolumeThreshold = 0.10;
@@ -15,8 +16,59 @@ var initialized = false;
 var device_id;
 
 window.onload = async () => {
-    fetchVolumeData();
+    const tuberDiv = document.getElementById('tuberdiv');
+    const tuberDivRoot = document.getElementById('tuberdivroot');
+    // const initialRootTransform = tuberDivRoot.style;
+    const initialRootTransform = "translate(-50%, 0%)";
+    console.log(initialRootTransform);
+
+    //Preload images (though there might be a better way than chaining them?)
+    ["styleBlink", "styleVolumeMedium", "styleVolumeSoft", "styleNeutral"].forEach( (c, i) => {
+        tuberDivRoot.style.transform = "translateX(-1000%)"; //This is here so it doesn't visibly go through all states. Using "display: none" defeats the purpose...
+        setTimeout( () => {
+            tuberDiv.className = c;
+            if (i >= 3) {
+                initialized = true;
+                tuberDivRoot.style.transform = initialRootTransform; //This is here so it doesn't visibly go through all states.
+            }
+        }, 70 * i);
+    });
     
+    const onFrame = () => {
+        fetchVolumeData();
+        
+        if (volume > mediumVolumeThreshold) {
+            if(tuberDiv.className != "styleVolumeMedium") {
+                //TODO: Pick random classes
+                tuberDiv.className = "styleVolumeMedium";
+            }
+        } else if (volume > softVolumeThreshold) {
+            if(tuberDiv.className != "styleVolumeSoft") {
+                tuberDiv.className = "styleVolumeSoft";
+            }
+        } else {
+            if (canBlink) {
+                canBlink = false;
+                if (tuberDiv.className != "styleBlink") {
+                    tuberDiv.className = "styleBlink";
+                    isBlinking = true;
+                        //We don't know how long the blink state is. Let's just assume it's going to be at most 1000ms
+                        setTimeout( () => {
+                            if(volume < softVolumeThreshold) {
+                                tuberDiv.className = "styleNeutral";
+                            }
+                            
+                            isBlinking = false;
+                        }, blinkStateDuration);
+                    }
+            } else { 
+                if(tuberDiv.className != "styleNeutral" && !isBlinking) {
+                    tuberDiv.className = "styleNeutral";
+                }
+            }
+        }
+    }
+    window.requestAnimationFrame(onFrame);
 };
 
 function fetchVolumeData() {
@@ -24,8 +76,19 @@ function fetchVolumeData() {
         .then(response => response.json())
         .then(data => {
             console.log('Received volume:', data.volume);
+            volume = data.volume;
         })
         .catch(error => {
             console.error('Error fetching volume:', error);
         });
+}
+
+//This method has a timer that runs a random amount of time and checks if the avatar can blink.
+//This is better in this situation than running the timer after every blink.
+function startBlinkTimer() {
+    var duration = blinkTimeOut + ( Math.random() * blinkTimeOutFuzziness );
+    setTimeout( ()=> {
+        canBlink = true;
+        startBlinkTimer();
+    }, duration)
 }
